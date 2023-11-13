ed52699cc7f9143c6d24dd61cdde79ff2a30603d
Santirs10 <santiagorosanchez@outlook.es>
2023-11-13 16:07:44 +0100

    Ejercicio_4 Concatenar cadenas de Strings añadido

---
+++ Ejercicio_4.js
@@ -0,0 +1,14 @@
+//Realiza un script que pida cadenas de texto hasta que se pulse cancelar. Al salir con cancelar debe mostrarse todas las cadenas con un guion
+const sc = require("prompt-sync")({sigint: true});
+
+let cadena="";
+while(true){
+    let scanner = sc("Dame un texto, si deseas salir escriba 'cancelar'")
+    if(scanner==="cancelar"){
+        console.log("Las cadenas guardadas son: " +cadena);
+        
+        break;
+    }
+    cadena+="\n --"+scanner;
+
+}
